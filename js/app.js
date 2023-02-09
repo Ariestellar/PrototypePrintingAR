94e8bc8ea5bdd068f09b8a60875208b55a7786b2
Ariestellar <ahdrei47@gmail.com>
2023-02-09 12:56:39 +0300

    edit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/BuildARTest.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/PrototypePrintingAR.github.io.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
